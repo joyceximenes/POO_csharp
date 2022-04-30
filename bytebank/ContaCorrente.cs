de2fbbf3eaeed058bf76900cd8b714ee7c7cbe08
Joyce <joyce.ximenes4@gmail.com>
2022-04-29 22:54:06 -0300

    basico

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    internal class ContaCorrente
+    {     
+            public string titular;
+            public string conta;
+            public string agencia;
+            public double saldo;
+    }
+}
+
---
+++ bytebank/Program.cs
@@ -0,0 +1,12 @@
+﻿using bytebank;
+
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "Maria";
+conta1.agencia = "1310-x";
+conta1.saldo = 300;
+
+Console.WriteLine("Titular: " + conta1.titular);
+Console.WriteLine("Agencia: " + conta1.agencia);
+Console.WriteLine("Saldo: " + conta1.saldo);
+
+
